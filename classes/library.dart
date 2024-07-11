4a3228e631450e76dd1df5cdc12ffc34b8585ecd
sm-aminul-islam <robinraja42@gmail.com>
2024-07-11 19:58:35 +0600

    Complete basic library management using OOP

--- classes/library.dart
+++ classes/library.dart
@@ -1,5 +1,6 @@
 import 'book.dart';
 import 'loan.dart';
+import 'member.dart';
 
 class Library {
   List<Book> books = [];
@@ -8,11 +9,28 @@ class Library {
     books.add(book);
   }
 
+  void loanBook(Book book, Member member) {
+    if (book.isAvailable) {
+      book.isAvailable = false;
+      loan.add(Loan(book, member, DateTime.now()));
+    } else {
+      print('${book.title} is not available');
+    }
+  }
+
   void listBook() {
     print("-----List All Books in Library----");
     for (var book in books) {
       print(
           'Title:${book.title},Author : ${book.author}, ISBN : ${book.isbn}, Available : ${book.isAvailable}');
+    }
+  }
+
+  void listLoanBook() {
+    print("-----List All LoanBooks in Library----");
+    for (var loans in loan) {
+      print(
+          'Title:${loans.book.title},Author : ${loans.book.author}, ISBN : ${loans.book.isbn}, Available : ${loans.book.isAvailable}');
     }
   }
 }
--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'classes/book.dart';
 import 'classes/library.dart';
+import 'classes/member.dart';
 
 void main() {
   Book book1 = Book('1984', "George Orwell", "1243", true);
@@ -9,4 +10,9 @@ void main() {
   library.addBook(book1);
   library.addBook(book2);
   library.listBook();
+
+  Member member = Member("Robin", "P2s4");
+
+  library.loanBook(book1, member);
+  library.listLoanBook();
 }
